b5d4412a3cc464f66f412a2de610397fd7356c3c
IqbalFebrianwar <zonelyrics26@gmail.com>
2024-02-08 15:36:08 +0700

    Feat : Fix Bug Onclick navbar

--- src/components/home/navbarUI.tsx
+++ src/components/home/navbarUI.tsx
@@ -5,9 +5,7 @@ import CloseIcon from "@/components/svg/close.svg";
 import { useState } from "react";
 
 const NavbarUI = () => {
-  const [Open, setOpen] = useState(true);
-  const [Close, setClose] = useState(false);
-  
+  const [Open, setOpen] = useState(true);  
   return (
     <>
       <div className="navbar justify-between top-0 z-20 lg:px-40 max-md:px-10 py-5 backdrop-filter fixed backdrop-blur-3xl">
@@ -43,7 +41,7 @@ const NavbarUI = () => {
                   height={40}
                 />
                 <Image
-                onClick={() => setClose(!Close)}
+                onClick={() => setOpen(!Open)}
                   className="max-lg:w-6 swap-on fill-current lg:hidden"
                   alt="close"
                   src={CloseIcon}
@@ -53,7 +51,7 @@ const NavbarUI = () => {
           </label>
         </div>
       </div>
-      {!Open ? (
+      {!Open && (
         <div className="bg-transparent max-w-xl rounded-full right-6 end-0 flex p-3 z-40 top-0 mt-24 backdrop-filter fixed backdrop-blur-3xl">
           <button className="btn lg:hidden text-gray-700 hover:bg-transparent shadow-none hover:text-blue-700 active:text-blue-700 active:bg-transparent bg-transparent border-0">
             Home
@@ -68,7 +66,7 @@ const NavbarUI = () => {
             Projects
           </button>
         </div>
-      ) : Close}
+      )}
     </>
   );
 };
